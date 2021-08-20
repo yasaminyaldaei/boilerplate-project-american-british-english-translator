462e89e14078deaf104ba1324bec2380abaf55d2
Yasamin Yaldaei <yasaminyaldaei@gmail.com>
2021-08-21 02:52:36 +0430

    add some other translators

--- components/translator.js
+++ components/translator.js
@@ -4,38 +4,59 @@ const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
 class Translator {
-    constructor() {
-        this.translated = "";
-    }
 
     translate(text, locale) {
-        this.translated = text;
-        return this.checkSpelling(text)
-            .checkAmericanOnlyWords(text)
-            .translated;
+       const first = this.checkSpelling(text);
+       const second = this.checkAmericanOnlyWords(first);
+       const third = this.checkAmericanToBritishTitles(second);
+       const fourth = this.checkTime(third);
+       const fifth = this.checkBritishOnlyWords(fourth);
 
+        return fifth;
     }
 
     checkSpelling(text) {
         for (const word in americanToBritishSpelling) {
-            if (text.includes(word)) {
-                 this.translated = text.replace(word, americanToBritishSpelling[word]);
-                 break;
-            }
+            text = text.replace(new RegExp(`\\b${word}\\b`, 'i'), americanToBritishSpelling[word]);
         }
 
-        return this;
+        return text;
     }
 
     checkAmericanOnlyWords(text) {
         for (const word in americanOnly) {
-            if (text.includes(word)) {
-                this.translated = text.replace(word, americanOnly[word]);
-                break;
-            }
+            text = text.replace(new RegExp(`\\b${word}\\b`, 'i'), americanOnly[word])
         }
 
-        return this;
+        return text;
+    }
+
+    checkAmericanToBritishTitles(text) {
+        for (const word in americanToBritishTitles) {
+            text = text.replace(new RegExp(`${word}`, 'i'), this.capitalize(americanToBritishTitles[word]))
+        }
+
+        return text;
+    }
+
+    checkTime(text) {
+        if (text.includes(":")) {
+            return text.replace(":", ".")
+        }
+
+        return text;
+    }
+
+    checkBritishOnlyWords(text) {
+        for (const word in britishOnly) {
+            text = text.replace(new RegExp(`\\b${word}\\b`, 'i'), britishOnly[word])
+        }
+
+        return text;
+    }
+
+    capitalize(text) {
+        return text[0].toUpperCase() + text.slice(1)
     }
 }
 
